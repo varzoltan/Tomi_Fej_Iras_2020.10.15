bbf1f8bb4921c7098a88920408f86e1d21d4000f
varzoltan <64203391+varzoltan@users.noreply.github.com>
2020-10-15 13:44:15 +0200

    Initial commit

---
+++ Tomi_Fej_Iras_2020.10.15/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tomi_Fej_Iras_2020._10._15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

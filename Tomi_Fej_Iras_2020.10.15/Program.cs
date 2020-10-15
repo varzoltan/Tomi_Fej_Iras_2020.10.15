24e55806375037653859aa93579c6b5b7d5620a1
varzoltan <64203391+varzoltan@users.noreply.github.com>
2020-10-15 13:59:29 +0200

    Update Program.cs

--- Tomi_Fej_Iras_2020.10.15/Program.cs
+++ Tomi_Fej_Iras_2020.10.15/Program.cs
@@ -10,6 +10,22 @@ namespace Tomi_Fej_Iras_2020._10._15
     {
         static void Main(string[] args)
         {
+            //1.feladat
+            Random fejIras = new Random();
+            //Ha 1 = I; 2 = F
+            int veletlen = fejIras.Next(1,2);
+            Console.WriteLine("1. feladat");
+            if (veletlen == 1)
+            {
+                Console.WriteLine($"A pénzfeldobás eredménye: I");
+            }
+            else
+            {
+                Console.WriteLine($"A pénzfeldobás eredménye: F");
+            }
+            
+            //2.feladat
+
         }
     }
 }
